61b27dc4340e1110b77425a56fd8c9c101c87e7f
samsonowal <74383977+samsonowal@users.noreply.github.com>
2025-07-03 12:26:06 +0530

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,12 @@
+a=input('Enter the first number: ')
+b=input('Enter the second number: ')
+a=int(a)
+b=int(b)
+add=a + b
+sub=a - b
+mul=a * b
+div=a / b
+print('Addition= ',add)
+print('Subtraction= ',sub)
+print('Multiplication= ',mul)
+print('Division= ',div)
